db73e6e48cd3c16e249bda496267d4198cbe2bde
MarocHOST <108360908+MarocHOST@users.noreply.github.com>
2026-04-02 17:43:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,5 @@
-module.exports = {};
+const express = require('express');
+const app = require('./api/verify');
+
+// هذا السطر لإخبار Vercel أن ملف verify.js هو المسؤول عن التشغيل
+module.exports = app;
